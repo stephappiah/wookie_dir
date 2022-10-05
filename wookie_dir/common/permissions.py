be8beec2f15fd5ab026a34ab2765bd5b282c05db
stephappiah <appstephen8@gmail.com>
2022-10-05 10:11:55 +0000

    blacklist dart vader

--- wookie_dir/common/permissions.py
+++ wookie_dir/common/permissions.py
@@ -1,8 +1,17 @@
 from rest_framework.permissions import BasePermission
 
 class BookViewPermission(BasePermission):
+
+    def is_blacklisted_user(self, request):
+        user = request.user
+        return user.username in ['darth-vader']
     
     def has_permission(self, request, view):
         if request.method == 'GET':
             return True
+
+        # blacklisted users cannot perform post action
+        if request.method == 'POST' and self.is_blacklisted_user(request):
+            return False
+
         return bool(request.user and request.user.is_authenticated)
